f3b1a9d91601cdc0256f202436db428567713600
Zo3rb <ali.f90@outlook.com>
2023-05-10 08:43:25 +0300

    again MD not valid

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,10 +6,7 @@ import requests
 
 def count_words(subreddit, word_list, after=None, word_dict={}):
     """
-    recursive function that queries the Reddit API,
-    parses the title of all hot articles,
-    and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).
+    prints a sorted count of given keywords
 
     Args:
         subreddit (str): The name of the subreddit to query.
