4d62a76e21b1897c8d1056bbaba14fa2f4e1ccbd
Zo3rb <ali.f90@outlook.com>
2023-05-10 08:38:39 +0300

    MD again

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
-"""
-recursive function that queries the Reddit API,
-parses the title of all hot articles,
-and prints a sorted count of given keywords.
-"""
+"""Contains count_words Function"""
 
 import requests
 
